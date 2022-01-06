66f65571a2599ca6ea757df13fe747627d0d73e1
Emre BULAK <emrbrnn@gmail.com>
2022-01-06 17:37:46 +0300

    Mongoose created and Photos added

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
 
+const Photo = require('./models/Photo');
+
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set('view engine', 'ejs');
 //Template Engine
 
+//Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//Middleware
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -23,10 +39,14 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
+
 app.get('*', (req, res) => {
   res.render('index');
 });
-
 
 const port = 3000;
 app.listen(port, () => {
